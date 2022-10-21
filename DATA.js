d06fef6d617e105ef26de4220cbc763141e157eb
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-21 15:32:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "6e3108bd-0a0c-4848-9e2e-326560c614e9",
+    "home_feed": [
+        {
+            "id": "77a5895a-34ca-42d9-917c-f686e5e3c804",
+            "artist_name": "focus",
+            "home_carousel": [
+                {
+                    "id": "3f8836a9-22c9-40f6-bbc4-bf94ede084f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "9bc4aa11-c155-423c-81e6-a7e57ad737a0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "e30baf77-c21d-41f7-a362-4b278f0d67df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png"
+                },
+                {
+                    "id": "1c4d2441-3b07-4d68-b214-bd90f4b196c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                },
+                {
+                    "id": "af77e29d-6ac9-4d41-a790-6a2625726fde",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                }
+            ],
+            "art_caption": "usually",
+            "art_description": "Line his cover customer. Organization quite manage enjoy worker."
+        },
+        {
+            "id": "8c273045-b0bf-4d72-b71d-1ad17787f984",
+            "artist_name": "travel",
+            "home_carousel": [
+                {
+                    "id": "e025d80e-5e2d-4b2a-b94b-3154c59f526d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                },
+                {
+                    "id": "05ff2613-d63b-43d3-bae1-d8649ad37679",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "40568b05-6b35-4532-aa60-ce6af36631fe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "c34f2e91-6f6e-4310-9461-4dc5f40ea6f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png"
+                },
+                {
+                    "id": "e1cbd16f-360f-4b14-abac-07f18276f941",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                }
+            ],
+            "art_caption": "beyond",
+            "art_description": "Expect stop continue keep."
+        },
+        {
+            "id": "b2a6202f-f7e9-432a-b1b7-69212b58df4d",
+            "artist_name": "Congress",
+            "home_carousel": [
+                {
+                    "id": "b35fe2d1-8b19-4118-8cb7-4e51a6cbd673",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "85bf6074-1958-4f0f-8c3d-a83c28e79a78",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                },
+                {
+                    "id": "e97e1ce7-02ec-4252-8272-151c90b8b021",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "ff2f3933-9943-49ca-befa-129a2cd235d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "5edede50-b2b3-40b2-86b1-fa67c7f242ba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                }
+            ],
+            "art_caption": "say",
+            "art_description": "Better yes mission. Nice same sort physical manager sit traditional interesting. Along together build space spring purpose."
+        },
+        {
+            "id": "a3590afc-380d-485a-988a-1903aaa8fa05",
+            "artist_name": "statement",
+            "home_carousel": [
+                {
+                    "id": "86219515-222c-46e6-ae15-f7fcbd6f7496",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "3e474bb6-a3b0-43ed-bd32-0ff6a472881e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                },
+                {
+                    "id": "e6fe0a9a-1647-4abc-8778-59de94fcb7c0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                },
+                {
+                    "id": "97d3d0f0-841e-4571-abb7-9001fb626d74",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                },
+                {
+                    "id": "6655b16f-df6e-4399-b434-bbe7a8c03915",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                }
+            ],
+            "art_caption": "second",
+            "art_description": "Could arrive consumer either according couple child. See language today owner group rise thought. Field agency sound share."
+        },
+        {
+            "id": "502a179b-8770-4060-8b2c-ba112ff2aaf8",
+            "artist_name": "off",
+            "home_carousel": [
+                {
+                    "id": "50513523-9f48-45f9-b932-48542b4410dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "ff1add95-1951-4921-8972-b9d655e764b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "21f59180-4567-4520-864c-6a5ac1e5b4fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "5184ca80-ac31-4557-a532-f90b4c5fd5f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                },
+                {
+                    "id": "99eecb3e-4141-4237-9e56-b9c6f0b7c3d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                }
+            ],
+            "art_caption": "lay",
+            "art_description": "Past fly everybody cold him type among. Whatever less its camera seem rather actually under. Answer why pass agree drive. Interview audience line magazine her serious."
+        }
+    ]
+}
